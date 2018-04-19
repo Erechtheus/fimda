aacfc0432f4a4dc5e476ff1beb20035f8b01dca8
Arne Binder <arne.b.binder@gmail.com>
2018-04-19 13:16:22 +0200

    prepared release

--- src/main/java/de/dfki/lt/fimda/fimda/FIMDA.java
+++ src/main/java/de/dfki/lt/fimda/fimda/FIMDA.java
@@ -40,7 +40,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 public class FIMDA {
 
